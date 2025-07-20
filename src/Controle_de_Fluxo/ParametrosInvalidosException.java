34f931851139f927466842bf22c2071e17056cd4
Isaachbt <isaac.silva1478@gmail.com>
2025-07-20 20:41:18 -0300

    feat: nova class para contar valores em intervalo no for.

---
+++ src/Controle_de_Fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Controle_de_Fluxo;
+
+public class ParametrosInvalidosException extends RuntimeException {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
